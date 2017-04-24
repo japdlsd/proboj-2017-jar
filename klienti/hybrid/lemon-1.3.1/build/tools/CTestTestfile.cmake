6b3db221081d9ec5a7d6c0658ee48a053ea691bc
Askar Gafurov <askar.gafurov@ksp.sk>
2017-04-24 22:33:44 +0200

    Added hybrid solution

---
+++ klienti/hybrid/lemon-1.3.1/build/tools/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/askar/projects/proboj-2017-jar/klienti/edmonds_movement/lemon-1.3.1/tools
+# Build directory: /home/askar/projects/proboj-2017-jar/klienti/edmonds_movement/lemon-1.3.1/build/tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
